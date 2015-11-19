a62477c04e4d1aa5d63eee71f345269c4a392218
Rob Forman <rob.forman@salesloft.com>
2015-11-19 06:41:06 -0500

    make it do the real FizzBuzz thing

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -2,7 +2,15 @@ class FizzBuzz
   def run
     ret_val = []
     (1..10).each do |num|
-      ret_val << "#{num}"
+      if num % 5 == 0 and num % 3 == 0
+        ret_val << "FizzBuzz"
+      elsif num % 5 == 0
+        ret_val << "Buzz"
+      elsif num % 3 == 0
+        ret_val << "Fizz"
+      else
+        ret_val << "#{num}"
+      end
     end
     ret_val
   end
