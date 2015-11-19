037b3088e6428fbdfe933e9f21ade068c969fef1
Rob Forman <rob.forman@salesloft.com>
2015-11-19 06:48:38 -0500

    refactor transform

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -2,16 +2,20 @@ class FizzBuzz
   def run
     ret_val = []
     (1..100).each do |num|
-      if num % 5 == 0 and num % 3 == 0
-        ret_val << "FizzBuzz"
-      elsif num % 5 == 0
-        ret_val << "Buzz"
-      elsif num % 3 == 0
-        ret_val << "Fizz"
-      else
-        ret_val << "#{num}"
-      end
+      ret_val << transform(num)
     end
     ret_val
+  end
+
+  def transform(i)
+    if i % 5 == 0 and i % 3 == 0
+      "FizzBuzz"
+    elsif i % 5 == 0
+      "Buzz"
+    elsif i % 3 == 0
+      "Fizz"
+    else
+      "#{i}"
+    end
   end
 end
